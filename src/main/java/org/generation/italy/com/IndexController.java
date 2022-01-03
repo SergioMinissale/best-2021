65c9dcee55b62d757c7467ad0000fe60191569ac
Sergio Minissale <minissale.sergio@outlook.itm>
2022-01-03 16:34:45 +0100

    Esercizio finito

--- src/main/java/org/generation/italy/com/IndexController.java
+++ src/main/java/org/generation/italy/com/IndexController.java
@@ -1,7 +1,17 @@
 package org.generation.italy.com;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
 
 public class IndexController {
 	@GetMapping("/Best of 2021 by")
@@ -9,5 +19,20 @@ public class IndexController {
 	public String Title() {
 		String name = "Sergio";
 		return "<html><body><h1>Best of 2021 by" + name + "</h1></body></html>";
+	}
+
+	@GetMapping("\songs")
+	public String songs(Model model) {
+		List<String> songsList = new ArrayList<String>();
+		model.addAttribute("songs", Arrays.asList("Come me, Achille Lauro", "InfinityLove, Marracash feat. Guè",
+				"Lucciole, Blanco", "Noi, Marracash", "Come nelle canzoni, Coez"));
+		return "songs";
+	}
+
+	@GetMapping("\movies")
+	public String movies(Model model) {
+		List<String> moviesList = new ArrayList<String>();
+		model.addAttribute("movies", Arrays.asList("SpiderMan: No Way Home", "Dune", "Eternals", "Don't look up", "Encanto"));
+		return "movies";
 	}
 }
